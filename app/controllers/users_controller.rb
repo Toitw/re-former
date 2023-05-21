22ad385adb187e521d553ad41c7f4e06eb0cb6fa
juroga_new <juanjrgast@gmail.com>
2023-05-21 12:48:10 +0200

    Completed the Users controller, input succesfully a new entry on the forms using only raw html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to users_path(@user)
+        else
+            render :new, status: :unprocesable_entity
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
